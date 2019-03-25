907a2b499e2afc22fef69d1df217a5e8d7d977c6
X00149064 <x00149064@COMPUTING.STU.IT-TALLAGHT.IE>
2019-03-25 11:16:51 +0000

    some changes

--- project/js/main.js
+++ project/js/main.js
@@ -1,11 +1,19 @@
-
+counter =0;
 function myFunction() {
+  counter++;
+
   // use jQuery ($ is shorthand) to find the div on the page and then change the html
   // jQuery can do a lot of crazy stuff so make sure to google around to find out more
 
-  $("#demo").html("NEWWW PARAGRAPH #javascript #fire");
+  $("#demo").html("NEWWW PARAGRAPH #javascript #fire #lit #theBest");
 
   // 'img-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
-  $("#doge-image").append(`<img class="img-circle" src="images/wowdoge.jpeg" />`);
+
+  if(counter>=10){
+    $("#demo").html("ENOUGH IS ENOUGH!!!!!!");
+  }
+  else if(counter<=10){
+    $("#doge-image").append(`<img class="img-circle" src="images/wowdoge.jpeg" />`);
+  }
 }
 
